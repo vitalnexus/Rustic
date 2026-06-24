d27ec3b7bc3c5bc594db95e8b24de88f0bc728b4
old man winter <noreply@vitalnexus.com>
2026-06-23 20:21:49 -0700

    Add incremental build artifacts for guessing game project
    - Created new work-products binary for incremental compilation.
    - Added lock file for incremental build tracking.

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,15 +1,39 @@
+use std::cmp::Ordering;
 use std::io;
+
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let mut guess = String::new(); // type with function new
+    // hides the secret number println!("The secret number is: {secret_number}");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {    
+        println!("Please input your guess.");
 
-    println!("You guessed: {guess}");
+        let mut guess = String::new(); // type with function new
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // trim removes whitespace at start and end of a string, and also removes newline \n and in Windows - the carriage return and newline  \r\n
+        let guess: u32 = match guess.trim().parse()  {  // parse converts a type to another type   
+            Ok(num) => num,   
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {   // expression is made up of arms. An arm consists of pattern to match against.
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }     
+        }
+    }
 }
