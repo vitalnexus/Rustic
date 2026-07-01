68e7d5255944be01d0e27117c01c8244718c3a4a
old man winter <noreply@vitalnexus.com>
2026-07-01 15:00:35 -0700

    Refactor mileage input handling and add initial Cargo.toml and main.rs for functions project

--- projects/miletrack/src/main.rs
+++ projects/miletrack/src/main.rs
@@ -8,28 +8,26 @@ fn main() {
         let mut mileage_s = String::new();
     
         io::stdin() 
-            .read_line(&mut mileage_s)
-            .expect("Cannot accept mileage!");
+            .read_line(&mut mileage_s).expect("Cannot accept mileage!");
 
         let mileage_s: u32 = match mileage_s.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };    
-        println!("Start miles is: {mileage_s}");
+        // println!("Start miles is: {mileage_s}");
 
         println!("Enter ending miles:");
         let mut mileage_e = String::new();
        
         io::stdin()
-           .read_line(&mut mileage_e)
-           .expect("Cannot accept mileage!");
+           .read_line(&mut mileage_e).expect("Cannot accept mileage!");
     
         let mileage_e: u32 = match mileage_e.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
-        println!("End miles is: {mileage_e}");
-   
+        //println!("End miles is: {mileage_e}");
+    
         let total_miles: u32 = {mileage_e} - {mileage_s};
  
         println!("Mileage driven is: {total_miles}");
